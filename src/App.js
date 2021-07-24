ccb202ca152117bdaf3c5ef72bd850e489aecd13
Medea Pruidze <medeapruidze3@gmail.com>
2021-07-24 16:54:52 +0300

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { useState } from "react";
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
-import people from "./data";
+import data from "./data";
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
   return (
-    <div>slider app</div>
+    <section className="section">
+      <div className="title">
+        <h2><span>/</span>reviews</h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, title, name, quote } = person;
+
+          //more stuff coming here
+
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="quote">{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          )
+        })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
 
   )
 }
