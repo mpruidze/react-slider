f4674f3826941e349f67a3f06b6774b80308c05d
Medea Pruidze <medeapruidze3@gmail.com>
2021-08-24 00:14:56 +0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from "./data";
@@ -6,6 +6,20 @@ import data from "./data";
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if(index < 0 ) setIndex(people.length - 1);
+    if(index > people.length-1) setIndex(0);      
+  }, [index,people]);
+
+  useEffect(() => {
+    let slider= setInterval(()=> {
+      setIndex(index + 1);
+    }, 3000)
+    return () => {
+      clearInterval(slider)
+    }
+  }, [index]);
 
   return (
     <section className="section">
@@ -16,10 +30,14 @@ function App() {
         {people.map((person, personIndex) => {
           const { id, image, title, name, quote } = person;
 
-          //more stuff coming here
+          let position = 'nextSlide';
+          if(personIndex === index) position = 'activeSlide';
+          if(personIndex === index-1 || 
+            (index === 0 && personIndex === people.length - 1)) 
+            position = 'lastSlide';
 
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className='person-img' />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -28,10 +46,10 @@ function App() {
             </article>
           )
         })}
-        <button className="prev">
+        <button className="prev" onClick={()=> setIndex(index-1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={()=> setIndex(index+1)}>
           <FiChevronRight />
         </button>
       </div>
